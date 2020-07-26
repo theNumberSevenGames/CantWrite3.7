dce8e76e7ed4ac4639928a1b77c232503c697766
theNumberSeven <thenumbersevengames@gmail.com>
2020-07-26 18:41:56 -0400

    Button/Camera movement
    Button text reacts to mouse hover, clicking moves camera correctly.

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Vector3[] buttonVectors;
+    public Camera mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void moveCamera()
+    {
+        mainCam.transform.position = mainCam.WorldToViewportPoint(buttonVectors[0]);
+        //mainCam.transform.LookAt(mainCam.WorldToScreenPoint(buttonVectors[0]));
+    }
+}
---
+++ Assets/Resources/Scripts/OnHoverScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using TMPro;
+public class OnHoverScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public UnityEvent OnHover = new UnityEvent();
+
+    public string buttonText;
+    
+    public TMP_Text thisText;
+    public GameObject[] targets;
+
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+         thisText.SetText(buttonText);
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+         thisText.SetText("");
+    }
+    void Start()
+    {
+        thisText.SetText("");
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
